570ea002e3dc87c32c5fc6c4299a4f480c37ba73
norrisasd <norrishipolito@gmail.com>
2023-11-20 15:44:46 +0800

    added navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { GiTicket } from "react-icons/gi";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", path: "/" },
+    { label: "Tickets", path: "/tickets" },
+    { label: "My Task", path: "/my-task" },
+    { label: "Help", path: "/help" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <GiTicket className="text-3xl" />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map(({ label, path }) => (
+          <li
+            key={path}
+            className={classnames({
+              "text-xl": true,
+              "font-medium": true,
+              "text-zinc-900": currentPath === path,
+              "text-zinc-500": currentPath !== path,
+              "hover:text-zinc-800": true,
+            })}
+          >
+            <Link href={path}>{label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
